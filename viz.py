fa10c09ed62d0676ee8754bbde7a9053d1469395
Greg Sadetsky <lepetitg@gmail.com>
2022-07-07 14:46:40 -0400

    updated version, hopefully improving graph

--- viz.py
+++ viz.py
@@ -26,18 +26,23 @@ with open(DATA_PATH) as f:
     })
 
 df = pd.DataFrame(all_rows)
+
 # taken from
 # https://github.com/colinmorris/pejorative-compounds/blob/master/heatmap.py
 df = df.pivot('pre', 'suff', 'count')
+df.fillna(0, inplace=True)
+
+# per reddit request
+# df.to_csv(CURR_DIR / 'data-pivoted.csv', float_format='%.0f', na_rep=0)
 
 # these words did not seem verby-enough to my personal taste.
 # this is an editorial choice that I moderately stand behind.
-df.drop(labels=['even', 'amount', 'chance', 'front', 'happen', 'line', 'man', 'name', 'number', 'part', 'plan', 'point', 'post', 'power', 'reason', 'result', 'shit', 'stuff', 'team', 'time', 'well', 'while',], axis=0, inplace=True)
-df.drop(labels=['aback'], axis=1, inplace=True)
+df.drop(labels=['piece', 'pay', 'sort', 'even', 'amount', 'chance', 'front', 'happen', 'line', 'man', 'name', 'number', 'part', 'plan', 'point', 'post', 'power', 'reason', 'result', 'shit', 'stuff', 'team', 'time', 'well', 'while',], axis=0, inplace=True)
+df.drop(labels=['aside', 'along', 'among', 'aback', 'open', 'together', 'it', 'to', 'past', 'round', 'above', 'across', 'forward', 'way'], axis=1, inplace=True)
 
 # selects a small subset of all verbs
 # a different interesting subset happens when using .mean instead
-df = df[df.max(axis=1) > 14000000]
+df = df[df.max(axis=1) > 10500000]
 
 # reset index, otherwise indices appear in descending alpha order
 df = df.iloc[::-1]
@@ -49,7 +54,7 @@ ax = fig.add_subplot(1, 1, 1)
 
 plt.text(x=0.5, y=0.94, s="Phrasal Verbs on Reddit", fontsize=16, ha="center", transform=fig.transFigure)
 plt.text(x=0.1, y=0.875, s="""
-Frequency of phrasal verbs (e.g., "find out", "get over") in Reddit comments, 2005-2019. Viz by /u/gregsadetsky
+Frequency of phrasal verbs (e.g., "find out", "get over") in Reddit comments, 2005-2019. Viz by /u/gregsadetsky, version 0.2
 Made using the public dataset on BigQuery. List of phrasal verbs from Wiktionary
 Thanks to /u/halfeatenscone for the inspiring "Rude Compounds" and to J. for the data processing help
 """.strip(), fontsize=5, ha="left", transform=fig.transFigure)
@@ -58,6 +63,13 @@ plt.subplots_adjust(top=0.75)
 plot = plt.pcolor(df, norm=colors.LogNorm(clip=True))
 # make it square, thanks to seaborn for the tip
 ax.set_aspect("equal")
+
+
+# based on feedback from reddit users and a suggestion by /u/halfeatenscone
+for i in range(1, df.shape[1]):
+  ax.axvline(i, color='white', lw=0.8)
+for i in range(1, df.shape[0]):
+  ax.axhline(i, color='white', lw=0.8)
 
 
 # from https://stackoverflow.com/a/7039989
